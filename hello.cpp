b1777f93180229acd06b84aa54a918eb630b6350
harsh <harshbanerjee30@gmail.com>
2024-08-31 18:31:23 +0530

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int linear(int arr[],int size, int key){
+    for(int i=0;i<size;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }return 69;//returning it if not found
+}
+
+int main(){
+    int count;
+    cout<<"Enter the number of integers in your array"<<endl;
+    cin>>count;
+    arr[count];
+    for(int i=0;i<count;i++){
+        cout<<"Enter all array integerss"<<endl;
+        cin>>arr[0]
+    }
+
+    int target;
+    cout<<"Enter your target"<<endl;
+    cin>>target;
+    
+    //now applting linear search on the algorithm 
+    
+    }
